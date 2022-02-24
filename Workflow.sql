49c6f7cc75c1f33c8fb329dc58ac7716572bae1c
Alexey Lonshakov <45521180+Phoenix795@users.noreply.github.com>
2022-02-24 15:04:52 +0600

    Find dashbords using some filters

--- Workflow.sql
+++ Workflow.sql
@@ -31,11 +31,22 @@ inner join portletconfiguration ON portalpage.id = portletconfiguration.portalpa
 inner join gadgetuserpreference ON portletconfiguration.id = gadgetuserpreference.portletconfiguration 
 where userprefvalue like any (array[
 '%customfield_id%', 
-....]);
+....
+]);
+
+--Find dashbords using some filters
+SELECT pp.id, pp.username, pp.pagename, gup.USERPREFVALUE FROM portalpage pp
+JOIN portletconfiguration pc on pp.id = pc.portalpage
+JOIN gadgetuserpreference gup on pc.ID = gup.portletconfiguration
+WHERE gup.USERPREFVALUE like any (array[
+'%filter-id%'
+ ....
+])
+order by pp.ID;
 
 --Find filters with columns using some customfields
 SELECT distinct SR.id, SR.filtername 
 FROM columnlayoutitem    CLI
 INNER JOIN columnlayout  CL  ON CLI.columnlayout = CL.id
 INNER JOIN searchrequest SR  ON CL.searchrequest = SR.id
-WHERE CLI.fieldidentifier IN ('customfield_id')
+WHERE CLI.fieldidentifier IN ('customfield_id');
