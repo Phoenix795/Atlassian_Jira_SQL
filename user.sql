6531b698f9fa0c895df5da8c426abc217c3a072a
Alexey Lonshakov <45521180+Phoenix795@users.noreply.github.com>
2024-04-26 23:16:34 +0200

    Update user.sql

--- user.sql
+++ user.sql
@@ -1,4 +1,4 @@
-//finding project roles by groups
+--finding project roles by groups
 select p.pkey as project_name, ,pr.name as role_name, cg.group_name as group_name
 from projectroleactor pra
 join project p on p.id = pra.pid
@@ -6,10 +6,20 @@ join projectrole pr on pr.id = pra.projectroleid
 join cwd_group cg on cg.group_name = pra.roletypeparameter
 join project p on p.id = pra.pid;
 
-//finding project roles by users
+--finding project roles by users
 select p.pname as project_name, pr.name as role_name, cu.display_name 
 from projectroleactor pra
 join projectrole pr on pr.id = pra.projectroleid
 join app_user au on au.user_key = pra.roletypeparameter
 join cwd_user cu on cu.lower_user_name = au.lower_user_name
 join project p on p.id = pra.pid;
+
+--getting user properties
+select
+  *
+from app_user au
+join propertyentry pe
+  on au.id = pe.entity_id
+  and pe.entity_name = 'ApplicationUser'
+join propertystring ps
+  on pe.id = ps.id;
